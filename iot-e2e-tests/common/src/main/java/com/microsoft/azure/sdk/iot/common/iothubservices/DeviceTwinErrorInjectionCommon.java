5e9c245aa51696cb745c2938b6591828603c52e1
timtay-microsoft <timtay@microsoft.com>
2018-10-12 11:37:51 -0700

    fix compilation error

--- iot-e2e-tests/common/src/main/java/com/microsoft/azure/sdk/iot/common/iothubservices/DeviceTwinErrorInjectionCommon.java
+++ iot-e2e-tests/common/src/main/java/com/microsoft/azure/sdk/iot/common/iothubservices/DeviceTwinErrorInjectionCommon.java
@@ -1309,4 +1309,48 @@ public class DeviceTwinErrorInjectionCommon extends MethodNameLoggingIntegration
         waitAndVerifyTwinStatusBecomesSuccess();
         waitAndVerifyDesiredPropertyCallback(PROPERTY_VALUE_UPDATE, true);
     }
+
+    private void testGetDeviceTwin() throws IOException, InterruptedException, IotHubException
+    {
+        // arrange
+        Map<Property, com.microsoft.azure.sdk.iot.device.DeviceTwin.Pair<TwinPropertyCallBack, Object>> desiredPropertiesCB = new HashMap<>();
+        for (int i = 0; i < MAX_PROPERTIES_TO_TEST; i++)
+        {
+            PropertyState propertyState = new PropertyState();
+            propertyState.property = new Property(PROPERTY_KEY + i, PROPERTY_VALUE);
+            deviceUnderTest.dCDeviceForTwin.propertyStateList.add(propertyState);
+            desiredPropertiesCB.put(propertyState.property, new com.microsoft.azure.sdk.iot.device.DeviceTwin.Pair<TwinPropertyCallBack, Object>(deviceUnderTest.dCOnProperty, propertyState));
+        }
+        internalClient.subscribeToTwinDesiredProperties(desiredPropertiesCB);
+        Thread.sleep(DELAY_BETWEEN_OPERATIONS);
+
+        Set<Pair> desiredProperties = new HashSet<>();
+        for (int i = 0; i < MAX_PROPERTIES_TO_TEST; i++)
+        {
+            desiredProperties.add(new Pair(PROPERTY_KEY + i, PROPERTY_VALUE_UPDATE + UUID.randomUUID()));
+        }
+        deviceUnderTest.sCDeviceForTwin.setDesiredProperties(desiredProperties);
+        sCDeviceTwin.updateTwin(deviceUnderTest.sCDeviceForTwin);
+        Thread.sleep(DELAY_BETWEEN_OPERATIONS);
+
+        for (PropertyState propertyState : deviceUnderTest.dCDeviceForTwin.propertyStateList)
+        {
+            propertyState.callBackTriggered = false;
+            propertyState.propertyNewVersion = -1;
+        }
+
+        // act
+        if (internalClient instanceof DeviceClient)
+        {
+            ((DeviceClient)internalClient).getDeviceTwin();
+        }
+        else
+        {
+            ((ModuleClient)internalClient).getTwin();
+        }
+
+        // assert
+        waitAndVerifyTwinStatusBecomesSuccess();
+        waitAndVerifyDesiredPropertyCallback(PROPERTY_VALUE_UPDATE, true);
+    }
 }
