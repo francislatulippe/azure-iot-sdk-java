a87b30dc3a5d0c2c9810240b8245d0b335721e89
timtay-microsoft <timtay@microsoft.com>
2018-10-15 11:16:02 -0700

    more fixes

--- iot-e2e-tests/jvm/src/test/java/tests/integration/com/microsoft/azure/sdk/iot/iothubservices/GetDeviceTwinErrorInjectionIT.java
+++ iot-e2e-tests/jvm/src/test/java/tests/integration/com/microsoft/azure/sdk/iot/iothubservices/GetDeviceTwinErrorInjectionIT.java
@@ -11,11 +11,22 @@ import com.microsoft.azure.sdk.iot.service.auth.AuthenticationType;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collection;
+
 @RunWith(Parameterized.class)
 public class GetDeviceTwinErrorInjectionIT extends GetDeviceTwinErrorInjectionCommon
 {
     public GetDeviceTwinErrorInjectionIT(String deviceId, String moduleId, IotHubClientProtocol protocol, AuthenticationType authenticationType, String clientType)
     {
         super(deviceId, moduleId, protocol, authenticationType, clientType);
+    }
+
+    //This function is run before even the @BeforeClass annotation, so it is used as the @BeforeClass method
+    @Parameterized.Parameters(name = "{2} with {3} auth using {4}")
+    public static Collection inputsCommons() throws IOException, GeneralSecurityException
+    {
+        return DeviceTwinIT.inputsCommons();
     }
 }
--- iot-e2e-tests/jvm/src/test/java/tests/integration/com/microsoft/azure/sdk/iot/iothubservices/SendReportedPropertiesErrorInjectionIT.java
+++ iot-e2e-tests/jvm/src/test/java/tests/integration/com/microsoft/azure/sdk/iot/iothubservices/SendReportedPropertiesErrorInjectionIT.java
@@ -5,11 +5,19 @@
 
 package tests.integration.com.microsoft.azure.sdk.iot.iothubservices;
 
+import com.microsoft.azure.sdk.iot.common.TestConstants;
+import com.microsoft.azure.sdk.iot.common.X509Cert;
+import com.microsoft.azure.sdk.iot.common.helpers.Tools;
+import com.microsoft.azure.sdk.iot.common.iothubservices.DeviceTwinCommon;
 import com.microsoft.azure.sdk.iot.common.iothubservices.SendReportedPropertiesErrorInjectionCommon;
 import com.microsoft.azure.sdk.iot.device.IotHubClientProtocol;
 import com.microsoft.azure.sdk.iot.service.auth.AuthenticationType;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collection;
 
 @RunWith(Parameterized.class)
 public class SendReportedPropertiesErrorInjectionIT extends SendReportedPropertiesErrorInjectionCommon
@@ -17,5 +25,12 @@ public class SendReportedPropertiesErrorInjectionIT extends SendReportedProperti
     public SendReportedPropertiesErrorInjectionIT(String deviceId, String moduleId, IotHubClientProtocol protocol, AuthenticationType authenticationType, String clientType)
     {
         super(deviceId, moduleId, protocol, authenticationType, clientType);
+    }
+
+    //This function is run before even the @BeforeClass annotation, so it is used as the @BeforeClass method
+    @Parameterized.Parameters(name = "{2} with {3} auth using {4}")
+    public static Collection inputsCommons() throws IOException, GeneralSecurityException
+    {
+        return DeviceTwinIT.inputsCommons();
     }
 }
